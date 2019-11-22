674362309b3826801751a29e9119427ca42dc09b
RamsesMartinez <ramsesmtzgzz@gmail.com>
2019-11-21 22:28:26 -0600

    Pruebas de la maestra y terminacion de la practica de BDD

---
+++ src/test/java/com/mayab/calidad/BDD/TestRunner.java
@@ -0,0 +1,13 @@
+package com.mayab.calidad.BDD;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="/home/cardinal/Documentos/CALIDAD SIRVE ESTE/Documents/TrabajoCardinal/Mars/ProyectoJava/features/SoundCloud_Play.feature"
+		)
+public class TestRunner {
+
+}
